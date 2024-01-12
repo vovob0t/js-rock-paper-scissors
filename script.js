98e43e370aa7e3009c0db341ad0f20c2e008aef5
vovob0t <vovobot20@gmail.com>
2024-01-12 15:37:01 +0500

    Add function for playing round, redone computer answer generation

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const toolsPull = ['Rock', 'Paper', 'Scissors'];
+const toolsPull = ['rock', 'paper', 'scissors'];
 
 function getRandomBetween0and2() {
     let randNumb = Math.floor((Math.random() + 0.1) * 10);
     while (randNumb === 10) {
         randNumb = Math.floor((Math.random() + 0.1) * 10);
     }
-    return randNumb%3;
+    return randNumb % 3;
 }
 
-function getComputerAnswer() {
+function getComputerInput() {
     let randNumb = getRandomBetween0and2();
     let compAnswer = toolsPull[randNumb];
     return compAnswer;
@@ -18,7 +18,7 @@ function testCompAnswers() {
     let numbPosib = [0, 0, 0, 0];
     let tempNumb
     for (i = 0; i < 100; i++) {
-        tempNumb = getComputerAnswer();
+        tempNumb = getComputerInput();
         console.log(tempNumb);
         switch (tempNumb) {
             case toolsPull[0]:
@@ -38,10 +38,59 @@ function testCompAnswers() {
     return numbPosib;
 }
 
-function playGame(userSelection, computerSelection){
-    if (userSelection === computerSelection){
-        return "Draw, you both played - " + computerSelection;
-    } else if (userSelection === toolsPull){
-        
+function playRound(userSelection, computerSelection) {
+    userSelection = userSelection.toLowerCase();
+    if (userSelection === computerSelection) {
+        return "Draw! You both played - " + computerSelection;
+    } else if (userSelection === toolsPull[0]) {
+        switch (computerSelection) {
+            case toolsPull[1]:
+                return "You lost! The round was - " + userSelection + " vs " + computerSelection;
+                break;
+            case toolsPull[2]:
+                return "You won! The round was - " + userSelection + " vs " + computerSelection;
+                break;
+        }
+    } else if (userSelection === toolsPull[1]) {
+        switch (computerSelection) {
+            case toolsPull[0]:
+                return "You won! The round was - " + userSelection + " vs " + computerSelection;
+                break;
+            case toolsPull[2]:
+                return "You lost! The round was - " + userSelection + " vs " + computerSelection;
+                break;
+        }
+    } else if (userSelection === toolsPull[2]) {
+        switch (computerSelection) {
+            case toolsPull[0]:
+                return "You lost! The round was - " + userSelection + " vs " + computerSelection;
+                break;
+            case toolsPull[2]:
+                return "You won! The round was - " + userSelection + " vs " + computerSelection;
+                break;
+        }
+    } else {
+        return "You answer is invalid! Please, choose between this tools: \n " + toolsPull;
     }
 }
+
+function getUserInput() {
+    let toolsPoolStr = toolsPull.toString().replaceAll(',', ', ');
+    let userTool = prompt("Choose your tool - " + toolsPoolStr);
+    userTool = userTool.toLowerCase();
+    while (!(toolsPull.includes(userTool))) {
+        userTool = prompt("Your input was incorrect! Please, choose from this tools - " + toolsPoolStr);
+        userTool = userTool.toLowerCase();
+    }
+    return userTool;
+}
+
+function game() {
+    let roundCount = 0;
+    let score = [0, 0];
+    let userTool = getUserInput();
+    let compTool = getComputerInput();
+    console.log(playRound(userTool, compTool));
+}
+
+game();
